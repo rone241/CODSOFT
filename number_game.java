954207a6aa23cdf170288c14a369626459ee45fe
Kíℜ๏ツ <112059158+rone241@users.noreply.github.com>
2023-08-08 23:46:11 +0530

    Update number_game.java

--- number_game.java
+++ number_game.java
@@ -8,32 +8,31 @@ public static void main(String[] args) {
 	int max=100;
 	
 	String str="y";
-	//System.out.println(guess);
-	System.out.println("Make a guess between "+min+" and "+max+" and you have 10 chances");
+	System.out.println("\n>> Make a guess between "+min+" and "+max+" and you have 10 chances <<");
 	while(str.equalsIgnoreCase(str)) {
 		int guess=s.nextInt(max-min)+min;
 	for(int i=1;i<=10;i++) {
 		
-		System.out.println("\u001B[39m enter your guess :");
+		System.out.print("\n\u001B[36m enter your guess :");
 		int get=scan.nextInt();
 		if(guess==get) {
-			System.out.println("\u001B[33m your guess is correct");
+			System.out.println("\n\t\u001B[33m your guess is correct");
 			System.out.println("your score is :"+(10-i));
 			break;
 		}
 		if(get>guess) {
-			System.out.println("\u001B[31m your guess is higher than random");
+			System.out.println("\u001B[31m your guess is too high... ");
 		}
 		else {
-			System.out.println("\u001B[31m your guess is lower than random");
+			System.out.println("\u001B[31m your guess is too low... ");
 		}
-		if(i==7) {
+		if(i==10) {
 			System.out.println("\u001B[33m random guess is :"+guess);
-			System.out.println("your score is :"+(10-i));
+			System.out.println(" your score is :"+(10-i));
 			break;
 		}
 	}
-	System.out.println("\nDo you wanna play again (y/n):");
+	System.out.println("\n\u001B[32mDo you wanna play again (y/n):");
 	scan.nextLine();
 	str=scan.nextLine();
 	}
